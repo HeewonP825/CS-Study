644541c15746622b7ae441509c02c7b04f1b3e7c
Jiseob Byeon <103080930+Jiseob-Byeon@users.noreply.github.com>
2023-08-28 21:55:24 +0900

    Update bj_2501(divisor).py
    풀이 방향 추가, 약수 구하기 알고리즘 반영

--- Algorithm/Jiseob/bj_2501(divisor).py
+++ Algorithm/Jiseob/bj_2501(divisor).py
@@ -1,6 +1,13 @@
+'''
+풀이 방향: 
+약수(N의 약수를 찾기 위해서는 N의 제곱근까지만 탐색하면 됨) 리스트 -> set 이용 같은 수 필터링 -> sort -> 인덱스 이용 출력
+'''
+
+from math import sqrt
+
 N, K = map(int, input().split())    # map, input, split -> N, K 받음
 divisors=[]                         # 약수 담을 리스트
-for i in range(1,(N//2)+1):         
+for i in range(1,int(sqrt(N))+1):         # 처음에는 N/2까지 탐색 -> 조사해보니 sqrt(N)까지만 탐색하면 됨
     if N%i == 0:
         divisors.append(i)          
         divisors.append(N//i)       # i로 나눈 몫도 약수로 추가
@@ -10,3 +17,8 @@ if len(divisors) < K:
     print("0")
 else:
     print(f"{divisors[K-1]}")
+
+'''
+References:
+1. https://doodle-ns.tistory.com/32 : 약수 구하기 알고리즘
+'''
